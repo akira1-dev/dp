cd1e90c27007650939c77d39bde14908affcc9ad
akira1-dev <bindgarenacod@gmail.com>
2025-11-21 15:59:54 +0800

    update

---
+++ public/js/temporyacc.js
@@ -0,0 +1,20 @@
+const loginBtn = document.getElementById('login-btn');
+loginBtn.addEventListener("click", () => {
+    const userUsernameInput = document.getElementById('user-username-input').value.trim();
+    const userPasswordInput = document.getElementById('user-input-password').value.trim();
+    const loginAlert = document.getElementById('login-alert');
+    
+    if (userUsernameInput === "test" && userPasswordInput === "1234") {
+        loginAlert.innerText = "Successfully Login!";
+        loginAlert.style.color = "green";
+        loginBtn.disabled = true;
+        loginBtn.innerText = "Wait...";
+        setTimeout(() => {
+            location.href = "home.html";
+        }, 3000);
+    } else {
+        loginAlert.innerText = "Valid Input.";
+        loginAlert.style.color = "red";
+    }
+    
+});
